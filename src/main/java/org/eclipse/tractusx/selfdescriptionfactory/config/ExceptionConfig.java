af40dcc9711571cce7926ed8441dfb3daae5fca1
dvasunin <dvasunin@gmail.com>
2022-11-25 17:36:03 +0200

    add license header

--- src/main/java/org/eclipse/tractusx/selfdescriptionfactory/config/ExceptionConfig.java
+++ src/main/java/org/eclipse/tractusx/selfdescriptionfactory/config/ExceptionConfig.java
@@ -1,3 +1,23 @@
+/********************************************************************************
+ * Copyright (c) 2021,2022 T-Systems International GmbH
+ * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 package org.eclipse.tractusx.selfdescriptionfactory.config;
 
 import org.springframework.boot.web.error.ErrorAttributeOptions;
@@ -32,12 +52,9 @@ public class ExceptionConfig {
             public Map<String ,Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
                 Map<String ,Object> defaultMap = super.getErrorAttributes(webRequest, options);
                 Map<String ,Object> errorAttributes = new LinkedHashMap<>();
-                // Customize.
-                // For eg: Only add the keys you want.
                 errorAttributes.put( KEY_STATUS, defaultMap.get( DEFAULT_KEY_STATUS ) );
                 errorAttributes.put( KEY_MESSAGE ,defaultMap.get( DEFAULT_KEY_MESSAGE ) );
                 errorAttributes.put( KEY_ERROR ,defaultMap.get( DEFAULT_KEY_ERROR ) );
-
 
                 return errorAttributes;
             }
