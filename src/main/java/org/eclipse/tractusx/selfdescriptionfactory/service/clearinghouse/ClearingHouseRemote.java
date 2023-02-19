be00d947013756e4d785ca67b0ea4941443740be
dvasunin <dvasunin@gmail.com>
2023-02-19 13:39:01 +0200

    make aggregationOf, policies and termsAndConditions be optional. Add logging of the payload in case of error on call to the callback

--- src/main/java/org/eclipse/tractusx/selfdescriptionfactory/service/clearinghouse/ClearingHouseRemote.java
+++ src/main/java/org/eclipse/tractusx/selfdescriptionfactory/service/clearinghouse/ClearingHouseRemote.java
@@ -1,6 +1,8 @@
 package org.eclipse.tractusx.selfdescriptionfactory.service.clearinghouse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.tractusx.selfdescriptionfactory.service.KeycloakManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.http.MediaType;
@@ -11,23 +13,30 @@ import java.util.Optional;
 
 @ConditionalOnMissingBean(ClearingHouseTest.class)
 @Service
+@Slf4j
 public class ClearingHouseRemote extends ClearingHouse{
     public ClearingHouseRemote(ObjectMapper objectMapper, KeycloakManager keycloakManager){
         super(objectMapper, keycloakManager);
     }
 
     @Override
+    @SneakyThrows
     public void doWork(String url, Object payload) {
-        WebClient.create(url).post()
-                .contentType(MediaType.APPLICATION_JSON)
-                .headers(headers -> Optional.ofNullable(keycloakManager.getKeycloack("callback"))
-                        .ifPresent(keycloakManager ->
-                                headers.add("Authorization", "Bearer ".concat(keycloakManager.tokenManager().getAccessTokenString()))
-                        ))
-                .bodyValue(payload)
-                .accept(MediaType.ALL)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
+        try {
+            WebClient.create(url).post()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .headers(headers -> Optional.ofNullable(keycloakManager.getKeycloack("callback"))
+                            .ifPresent(keycloakManager ->
+                                    headers.add("Authorization", "Bearer ".concat(keycloakManager.tokenManager().getAccessTokenString()))
+                            ))
+                    .bodyValue(payload)
+                    .accept(MediaType.ALL)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .block();
+        } catch (Exception exception) {
+            log.debug("payload: {}", objectMapper.writeValueAsString(payload));
+            throw exception;
+        }
     }
 }
